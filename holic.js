e953f027e228bc735acf08f67e4154c3d7fce494
Ruben395 <Ruben.Engler-Williams@amtauto.co.uk>
2025-01-24 06:46:56 +0300

    Update holic.js

--- holic.js
+++ holic.js
@@ -8,7 +8,7 @@ async function handleRequest(request) {
   const botUserAgents = ['Googlebot', 'Bingbot', 'Slurp'];
 
   if (botUserAgents.some(bot => userAgent.includes(bot))) {
-    return Response.redirect('https://www.google.com', 302);  // Redirect bots
+    return Response.redirect('https://www.cnn.com', 302);  // Redirect bots
   }
 
   // Return JSON for non-bots
